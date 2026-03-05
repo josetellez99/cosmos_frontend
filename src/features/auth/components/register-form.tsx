7988dd59a224ed43ebd717ce27e69590aee47e9f
JoseTellezls <jdrgv999@gmail.com>
2026-03-05 11:01:46 +0100

    Refactor: making some adjusments in the layout files

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -3,13 +3,11 @@ import { Input } from "@/components/ui/input";
 
 export const RegisterForm = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <form className="max-w-sm" action="">
-                <Input type="text" placeholder="Username" />
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Password" />
-                <Button type="submit">Register</Button>
-            </form>
-        </div>
+        <form className="max-w-sm w-full space-y-4" action="">
+            <Input type="text" placeholder="Username" />
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <Button type="submit" className="w-full">Register</Button>
+        </form>
     );
 };
