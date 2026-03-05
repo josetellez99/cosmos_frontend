70c4dc83d42be14721c9a53abb803009df4c76db
JoseTellezls <jdrgv999@gmail.com>
2026-03-05 11:15:47 +0100

    Feat: Adding the inputs needed for the form

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -4,10 +4,13 @@ import { Input } from "@/components/ui/input";
 export const RegisterForm = () => {
     return (
         <form className="max-w-sm w-full space-y-3" action="">
-            <Input type="text" placeholder="Username" />
-            <Input type="email" placeholder="Email" />
-            <Input type="password" placeholder="Password" />
-            <Button type="submit" className="w-full">Register</Button>
+            <Input type="text" placeholder="Maria" />
+            <Input type="text" placeholder="Rodriguez" />
+            <Input type="date" placeholder="24-02-1990" />
+            <Input type="email" placeholder="micorreo@dominio.com" />
+            <Input type="password" placeholder="********" />
+            <Input type="password" placeholder="********" />
+            <Button type="submit" className="w-full">Registrarse</Button>
         </form>
     );
 };
