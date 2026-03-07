73ffeade3e3a513622025b2156977d3f3b14ca0d
JoseTellezls <jdrgv999@gmail.com>
2026-03-07 13:15:09 +0100

    Small fixes

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,14 +1,16 @@
-import { registerUserService } from "@/features/auth/services/register";
-import { loginService } from "@/features/auth/services/login";
-import { AuthContext } from "@/contexts/AuthContext";
-import { useContext } from "react";
 import type { RegisterRequest } from "@/features/auth/types/request/register";
 import type { LoginRequest } from "@/features/auth/types/request/login";
 import type { ApiResponse } from "@/lib/apiResponses";
 import type { RegisterResponse } from "@/features/auth/types/response/register";
 import type { UserSession } from "@/features/auth/types/UserSession";
 import type { VerifyEmailRequest } from "@/features/auth/types/request/verify-email";
+
+import { registerUserService } from "@/features/auth/services/register";
 import { verifyEmailService } from "@/features/auth/services/verify-email-service";
+import { loginService } from "@/features/auth/services/login";
+
+import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react";
 
 export const useAuth = () => {
 
