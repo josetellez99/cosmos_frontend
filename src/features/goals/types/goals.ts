1280bd75fa68e054f8c6abdde343f00112287aee
JoseTellezls <jdrgv999@gmail.com>
2026-03-19 13:57:55 +0100

    Refactor: solving a linter error

--- src/features/goals/types/goals.ts
+++ src/features/goals/types/goals.ts
@@ -1,10 +1,12 @@
-export enum GoalOrderBy  {
-    NAME = "name",
-    CREATED_AT = "created_at",
-    DEADLINE = "deadline",
-    STARTING_DATE = "starting_date",
-    STATUS = "status",
-    TEMPORALITY = "temporality",
-    SORT_ORDER = "sort_order",
-    IS_SUBGOAL = "is_subgoal",
-}
+export const GoalOrderBy = {
+    NAME: "name",
+    CREATED_AT: "created_at",
+    DEADLINE: "deadline",
+    STARTING_DATE: "starting_date",
+    STATUS: "status",
+    TEMPORALITY: "temporality",
+    SORT_ORDER: "sort_order",
+    IS_SUBGOAL: "is_subgoal",
+} as const
+
+export type GoalOrderBy = typeof GoalOrderBy[keyof typeof GoalOrderBy]
