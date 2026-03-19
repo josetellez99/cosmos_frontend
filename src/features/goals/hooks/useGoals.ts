174b3de67f3c0f55c41e7a4406fe4412c3456605
JoseTellezls <jdrgv999@gmail.com>
2026-03-19 14:23:55 +0100

    Feat: Implementing the basic structure for the useGoals hook

--- src/features/goals/hooks/useGoals.ts
+++ src/features/goals/hooks/useGoals.ts
@@ -1,5 +1,32 @@
 import { useState } from "react";
 
+import type { GetUserGoalsRequest } from "@/features/goals/types/request/get-user-goals"
+import type { GoalSummaryResponse } from "@/features/goals/types/response/user-goals"
+import type { ApiResponse } from "@/lib/apiResponses"
+
+import { getUserGoalsService } from "@/features/goals/services/get-goals-service"
+
 export const useGoals = () => {
     
+    const [goals, setGoals] = useState<GoalSummaryResponse[]>([])
+    const [goalsError, setGoalsError] = useState<string>()
+
+    const getUserGoals = async (req?: GetUserGoalsRequest) => {
+        const response = await getUserGoalsService(req)
+        if(response.ok) {
+            setGoals(response.data)
+        } else {
+            setGoalsError(response.message)
+        }
+    }
+
+    return {
+        goals,
+        goalsError,
+        getUserGoals
+    }
 };
+
+// TODO: get all goals by default (status active, temporality yearly)
+// TODO: egt archieved goals
+// TODO: get goals by temporality
