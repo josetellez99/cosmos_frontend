4489473f2f7f01fef90cb6e533fa24ce8245ef97
JoseTellezls <jdrgv999@gmail.com>
2026-03-19 12:29:09 +0100

    Feat: Adding the goals endpoint to the app

--- src/lib/endpointsMap.ts
+++ src/lib/endpointsMap.ts
@@ -1,6 +1,9 @@
 import { API_URL } from "@/lib/constants"
 
 export const ENDPOINTS_MAP = {
+    GOALS: {
+        GET_USER_GOALS: `${API_URL}/goals`,
+    },
     AUTH: {
         REGISTER: `${API_URL}/auth/register`,
         LOGIN: `${API_URL}/auth/login`,
