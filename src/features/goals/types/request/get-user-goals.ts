b13cf68403f325ed91659485631da410d2bd14f3
JoseTellezls <jdrgv999@gmail.com>
2026-03-23 15:44:13 +0100

    Refactor: For the goals endpoint, we now pass params temporality and status as an array because we can have several options in the req

--- src/features/goals/types/request/get-user-goals.ts
+++ src/features/goals/types/request/get-user-goals.ts
@@ -2,10 +2,10 @@ import type { Database } from "@/types/database.types"
 import type { GoalOrderBy } from "@/features/goals/types/goals"
 
 export interface GetUserGoalsRequest {
-    temporality?: Database["public"]["Enums"]["goal_temporality_type"]
-    status?: Database["public"]["Enums"]["item_status_type"]
+    temporality?: Database["public"]["Enums"]["goal_temporality_type"][]
+    status?: Database["public"]["Enums"]["item_status_type"][]
     orderBy?: GoalOrderBy
-    order?: number
+    order?: 'ASC' | 'DESC'
     startDate?: string
     endDate?: string
 }
