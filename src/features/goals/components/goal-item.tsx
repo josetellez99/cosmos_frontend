bdb954501de2a4310831782b8e75826489a92925
JoseTellezls <jdrgv999@gmail.com>
2026-03-24 17:04:16 +0100

    Feat: Implementing the conditional rendering for the three goal items cases: not startes, in progress and completed

--- src/features/goals/components/goal-item.tsx
+++ src/features/goals/components/goal-item.tsx
@@ -6,7 +6,9 @@ import { Typography } from "@/components/ui/typography";
 import type { GoalSummaryResponse } from "@/features/goals/types/response/user-goals";
 import { strengthenColor } from "@/helpers/getStrengthenColor";
 import { appRoutes } from "@/lib/constants/routes";
-import { goalTemporality } from "@/lib/constants/temporality";
+import { goalTemporality } from "@/lib/constants/goals_temporalities";
+import { getNaturalFormatDate } from "@/helpers/dates"
+import { goalStatus } from "@/lib/constants/goals_status";
 
 interface props {
   goal: GoalSummaryResponse;
@@ -15,6 +17,8 @@ interface props {
 export const GoalItem = ({ goal }: props) => {
 
   const isYearly = goal.temporality === goalTemporality.YEAR;
+  const isNotStarted = goal.status === goalStatus.NOT_STARTED;
+  const isCompleted = goal.status === goalStatus.COMPLETED;
 
   const borderColor = isYearly ? strengthenColor(goal.color, 0.08) : undefined;
   const accentColor = isYearly ? strengthenColor(goal.color, 0.9) : undefined;
@@ -26,6 +30,7 @@ export const GoalItem = ({ goal }: props) => {
         className={cn(
           "p-4 default-card-rounded",
           !isYearly && "bg-white border border-gray-200",
+          isNotStarted && "opacity-60",
         )}
         style={
           isYearly
@@ -46,43 +51,58 @@ export const GoalItem = ({ goal }: props) => {
               {goal.name}
             </Typography>
           </div>
-          <div className="flex items-center gap-3">
-            <span
-              style={{
-                color: isYearly ? accentColor : "var(--primary)",
-                backgroundColor: "white",
-                borderRadius: "40%",
-                paddingInline: "8px",
-                paddingBlock: "4px",
-                display: "inline-flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              className={cn("text-xs font-semibold")}
-            >
-              {goal.progress}%
-            </span>
-          </div>
+
+          {!isNotStarted && (
+            <div className="flex items-center gap-3">
+              {isCompleted ? (
+                <span className="text-xs font-semibold text-green-600 flex items-center gap-1">
+                  <span className="animate-pulse">●</span> Completada
+                </span>
+              ) : (
+                <span
+                  style={{
+                    color: isYearly ? accentColor : "var(--primary)",
+                    backgroundColor: "white",
+                    borderRadius: "40%",
+                    paddingInline: "8px",
+                    paddingBlock: "4px",
+                    display: "inline-flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                  className={cn("text-xs font-semibold")}
+                >
+                  {goal.progress}%
+                </span>
+              )}
+            </div>
+          )}
         </div>
 
-        <div
-          className={cn("h-1.5 w-full rounded-full overflow-hidden mb-1")}
-          style={
-            isYearly
-              ? { backgroundColor: accentColor + "30" }
-              : { backgroundColor: "rgb(229, 231, 235)" }
-          }
-        >
+        {isNotStarted ? (
+          <p className="text-xs text-right">
+            Empieza el {getNaturalFormatDate(goal.startingDate)}
+          </p>
+        ) : (
           <div
-            className={cn(
-              "h-full transition-all duration-1000 ease-out rounded-full",
-            )}
-            style={{
-              width: `${goal.progress}%`,
-              backgroundColor: isYearly ? accentColor : "var(--primary)",
-            }}
-          />
-        </div>
+            className={cn("h-1.5 w-full rounded-full overflow-hidden mb-1")}
+            style={
+              isYearly
+                ? { backgroundColor: accentColor + "30" }
+                : { backgroundColor: "rgb(229, 231, 235)" }
+            }
+          >
+            <div
+              className={cn(
+                "h-full transition-all duration-1000 ease-out rounded-full",
+              )}
+              style={{
+                width: `${goal.progress}%`,
+                backgroundColor: isYearly ? accentColor : "var(--primary)",
+              }}
+            />
+          </div>
+        )}
       </div>
     </Link>
   );
