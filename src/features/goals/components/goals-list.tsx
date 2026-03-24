c595a987921c961349b393cda2ee874eb2c2197e
JoseTellezls <jdrgv999@gmail.com>
2026-03-24 16:19:01 +0100

    Feat: Adding the fallback message when received an empty list

--- src/features/goals/components/goals-list.tsx
+++ src/features/goals/components/goals-list.tsx
@@ -1,11 +1,24 @@
 import { GoalItem } from "@/features/goals/components/goal-item"
 import type { GoalSummaryResponse } from "@/features/goals/types/response/user-goals"
+import { FallbackMessage } from "@/components/ui/messages/fallback-message"
 
 interface props {
     goals: GoalSummaryResponse[]
+    fallbackMessage: string
 }
 
-export const GoalsList = ({ goals } : props) => {
+export const GoalsList = ({ goals, fallbackMessage } : props) => {
+
+    if (goals.length === 0) {
+        return (
+            <div>
+                <FallbackMessage>
+                    {fallbackMessage}
+                </FallbackMessage>
+            </div>
+        )
+    }
+
     return (
         <ul className="flex flex-col spacing-in-list-elements">
             {goals.map((goal) => (
