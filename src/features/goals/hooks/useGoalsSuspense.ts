405f9e8110a818c726df96b2421da090e9694654
JoseTellezls <jdrgv999@gmail.com>
2026-03-26 09:26:51 +0100

    Refactor: some small changes in the format of useGoalsSuspense

--- src/features/goals/hooks/useGoalsSuspense.ts
+++ src/features/goals/hooks/useGoalsSuspense.ts
@@ -5,13 +5,23 @@ import { getUserGoalsService } from '@/features/goals/services/get-goals-service
 import { goalQueryKeys } from '@/features/goals/helpers/queryKeys'
 
 export const useGoalsSuspense = (filters?: GetUserGoalsRequest) => {
+  
   const { data, refetch } = useSuspenseQuery({
     queryKey: goalQueryKeys.list(filters),
     queryFn: () => getUserGoalsService(filters),
     staleTime: 10 * 60 * 1000,
   })
 
-  const goals: GoalSummaryResponse[] = data?.ok ? data.data : []
+  let goals: GoalSummaryResponse[];
 
-  return { goals, refetch }
+  if(data && data.ok) {
+    goals = data.data
+  } else {
+    goals = []
+  }
+
+  return { 
+    goals, 
+    refetch 
+  }
 }
