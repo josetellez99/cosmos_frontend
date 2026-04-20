a508b39c6b1e14bb8c33d98099e9783b7f286bc4
JoseTellezls <jdrgv999@gmail.com>
2026-04-20 16:10:58 +0200

    Adding more console.log to debug

--- src/helpers/cookies.ts
+++ src/helpers/cookies.ts
@@ -1,5 +1,7 @@
 export function getCookie(name: string) : string {
     const cookie = document.cookie.split('; ').find(row => row.startsWith(`${name}=`))?.split('=')[1];
+    console.log('cookie csrf')
+    console.log(cookie)
     if (!cookie) {
         throw new Error(`Cookie ${name} not found`);
     }
