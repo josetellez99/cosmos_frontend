3571df0bfda037313ff48f6ae5971962d3ffe909
JoseTellezls <jdrgv999@gmail.com>
2026-05-07 13:04:24 +0200

    Bug: a problem with the date format has being resolved to be able of fetching all data correctly

--- src/features/habits/helpers/format-date-for-api.ts
+++ src/features/habits/helpers/format-date-for-api.ts
@@ -1,6 +1,6 @@
 export const formatDateForApi = (date: Date): string => {
     const year = date.getFullYear()
     const month = String(date.getMonth() + 1).padStart(2, '0')
-    const day = date.getDate()
+    const day = String(date.getDate()).padStart(2, '0')
     return `${year}-${month}-${day}`
 }
